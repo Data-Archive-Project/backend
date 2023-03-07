0c71d91945b2f58a7526c75ad6bf6dd346584036
Prince Jan Kumi <janprince002@gmail.com>
2023-03-07 02:56:49 +0000

    update models

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 
 class Role(models.Model):
@@ -34,10 +34,10 @@ class Document(models.Model):
 class Profile(models.Model):
     user  = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     title = models.CharField(max_length=255) # dr, prof, mr 
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,10}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone = models.CharField(max_length=17, validators=[phone_regex], blank=True)
     role = model.ForeignKey(Role, on_delete=models.CASCADE)
-    documents = models.ManyToManyField(Document, related_name="profiles", on_delete=models.CASCADE)
+    documents = models.ManyToManyField(Document, related_name="profiles", on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         pass

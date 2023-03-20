0245415b3e89ce830beba3b8b48e08ad35eba04a
Prince Jan Kumi <janprince002@gmail.com>
2023-03-20 11:45:41 +0000

    fix indentation

--- api/serializers.py
+++ api/serializers.py
@@ -3,24 +3,21 @@ from .models import *
 
 
 class LoginSerializer(serializers.Serializer):
-  """
-    To handle data auth credentials
-  """
-	email = serializers.EmailField()
-	password = serializers.CharField()
-	is_admin = serializers.BooleanField()
+    """
+      To handle data auth credentials
+    """
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    is_admin = serializers.BooleanField()
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = UserProfile
-    fields = ['user', 'rank', 'position', 'phone']
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'rank', 'position', 'phone']
 
-  
 
 class DocumentCategorySerializer(serializers.ModelSerializer):
-  class Meta:
-    model = DocumentCategory
-    fields = ["name", 'decription']
-
-
+    class Meta:
+        model = DocumentCategory
+        fields = ["name", 'decription']
